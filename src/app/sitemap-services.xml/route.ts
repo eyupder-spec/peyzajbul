b45f80f138b192de6bff1db7645a7eb653819ec8
eyupder-spec <eyupder@gmail.com>
2026-03-29 22:42:42 +0300

    fix: escape special characters in category sitemap XML

--- src/app/sitemap-services.xml/route.ts
+++ src/app/sitemap-services.xml/route.ts
@@ -1,6 +1,19 @@
 import { NextResponse } from 'next/server';
 import { getAllCategories } from '@/lib/categories';
 
+function escapeXml(unsafe: string) {
+  return unsafe.replace(/[<>&"']/g, (c) => {
+    switch (c) {
+      case '<': return '&lt;';
+      case '>': return '&gt;';
+      case '&': return '&amp;';
+      case '"': return '&quot;';
+      case "'": return '&apos;';
+      default: return c;
+    }
+  });
+}
+
 export async function GET() {
   const baseUrl = 'https://www.peyzajbul.com';
   const categories = getAllCategories();
@@ -24,8 +37,8 @@ export async function GET() {
     <priority>0.7</priority>
     <image:image>
       <image:loc>${baseUrl}${cat.imageUrl}</image:loc>
-      <image:title>${cat.label} - Peyzajbul</image:title>
-      <image:caption>${cat.shortDescription}</image:caption>
+      <image:title>${escapeXml(cat.label)} - Peyzajbul</image:title>
+      <image:caption>${escapeXml(cat.shortDescription)}</image:caption>
     </image:image>
   </url>`).join('')}
 </urlset>`;
